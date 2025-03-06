212bc59cccd574e258c896bff9db323726b23a05
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-03-06 17:38:03 -0300

    update fish and starship config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,9 @@ status is-interactive || exit
 
 if not functions -q fisher; curl -sL https://git.io/fisher | source && fisher update; end
 
+function fish_default_mode_prompt ;end # disable the vi mode view in prompt
+
+abbr i "awk '{print \$1}' .packages/yay.txt | xargs -n1 yay -Ss"
 abbr q 'exit'
 abbr s 'source'
 abbr c 'codium'
@@ -43,8 +46,7 @@ alias ignore='codium ~/.gitignore'
 alias dot='stow --stow'
 alias undot='stow --delete'
 alias redot='stow --restow'
-
-source $__fish_config_dir/let.fish
+alias pnpx='pnpm dlx'
 
 function __stow_completion
   if test -f ~/.stowrc
@@ -63,3 +65,62 @@ function __stow_completion
 end
 
 complete -f -c stow -a "(__stow_completion)"
+
+function vi
+  if set -q __vi_mode_enabled
+    set -e __vi_mode_enabled
+    fish_default_key_bindings
+    echo vi is disable
+  else
+    set -Ux __vi_mode_enabled 1
+    fish_vi_key_bindings
+    echo vi is enable
+  end
+end
+
+if [ $GREETING ]
+  function fish_greeting
+    fastfetch --config ~/.config/fastfetch/greeting.jsonc 2> /dev/null
+  end
+else
+  set -g fish_greeting
+end
+
+function fish_title
+  set -l current_dir (basename (pwd))
+  set -l parent_dir (basename (dirname (pwd)))
+
+  if test -n "$TMUX"
+    echo $parent_dir/$current_dir
+  else
+    switch $current_dir
+      case (whoami); echo 
+      case 'Documentos' 'Documents'; echo 󰈙
+      case 'Models' 'Modelos'; echo 󰛼
+      case 'Downloads'; echo 
+      case 'Music' 'Músicas'; echo 󰝚
+      case 'Pictures' 'Imagens'; echo 
+      case 'Projects' 'Developer' 'Dev'; echo 󰲋
+      case 'Games'; echo 󰮂
+      case 'Public' 'Público'; echo 󰖟
+      case 'Vídeos' 'Videos'; echo 
+      case 'Desktop' 'Área de trabalho'; echo 
+      case 'Dotfiles' 'Config' 'Configurations'; echo 
+      case 'Annotations' 'Notes'; echo 
+      case '*'; echo $parent_dir/$current_dir
+    end
+  end
+end
+
+switch $PROMPT
+  case "starship"
+    function starship_transient_prompt_func
+      echo -n (set_color blue)" "
+    end
+
+    starship init fish | source
+
+    enable_transience
+  case "ohmyposh"
+    oh-my-posh init --config ~/.config/ohmyposh/$OHMYPOSH_THEME.json fish | source
+end
