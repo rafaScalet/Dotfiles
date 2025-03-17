c015d0bc6ebee461f30e142163d37754f9180a15
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-03-16 23:14:23 -0300

    update fish and codium config

--- fish/dot-config/fish/conf.d/plugins.fish
+++ fish/dot-config/fish/conf.d/plugins.fish
@@ -2,15 +2,15 @@ status is-interactive || exit
 
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 
+# fundle plugin archelaus/shell-mommy
+# fundle plugin gazorby/fish-abbreviation-tips
+fundle plugin halostatue/fish-utils@v4
+fundle plugin jhillyerd/plugin-git
+# fundle plugin joehillen/to-fish
 fundle plugin jorgebucaran/autopair.fish
 fundle plugin jorgebucaran/replay.fish
-fundle plugin rafascalet/docker-db.fish
-fundle plugin jhillyerd/plugin-git
 fundle plugin nickeb96/puffer-fish
-fundle plugin gazorby/fish-abbreviation-tips
-fundle plugin halostatue/fish-utils@v4
-fundle plugin archelaus/shell-mommy
-fundle plugin joehillen/to-fish
+fundle plugin rafascalet/docker-db.fish
 
 fundle init
 
