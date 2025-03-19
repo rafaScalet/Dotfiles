47d0e25478387ad0c4c09fab0bb3a537425fe067
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-03-18 22:16:23 -0300

    update config for fish, git and codium

--- fish/dot-config/fish/completions/stow.fish
+++ fish/dot-config/fish/completions/stow.fish
@@ -1,18 +1,5 @@
 function __get_dotfiles_dir
-  set -g __stowrc_command string replace "dir=" "" (cat $stowrc | grep dir | cut -c3-)
-
-  if set -q $stowrc; and test -e $stowrc
-    $__stowrc_command
-  else if test -e ./.stowrc
-    eval set -Ux stowrc (pwd)/.stowrc
-    $__stowrc_command
-  else if test -e ~/.stowrc
-    eval set -Ux stowrc ~/.stowrc
-    $__stowrc_command
-  else
-    eval set -Ux stowrc (find ~ -name ".stowrc" -not -path '*/Trash/*')
-    $__stowrc_command
-  end
+  string replace "dir=" "" (cat ~/Dotfiles/.stowrc | grep dir | cut -c3-)
 end
 
 function __stow_completion
