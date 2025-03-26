166b2193af062369576f24d99115048c7e542ce1
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-03-26 10:17:13 -0300

    update config for starship
    add personal config for deno and bun modules

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
 
 stow --dir ~/Dotfiles \
-  --ignore="config" \
-  --ignore="templates" \
   --ignore="assets" \
+  --ignore="config" \
+  --ignore="docs" \
+  --ignore="examples" \
   --ignore="packages" \
+  --ignore="scripts" \
+  --ignore="templates" \
   --ignore="bootstrap.sh" \
   --ignore=".vscode" \
   --ignore=".git" \
